fb5eb2a2c67cc2d5f0932e7bd794083b008880b5
Isaac Sahle <isaacwsahle@gmail.com>
2019-06-12 16:49:44 -0700

    encode and output file using huffman tree.

--- huffman.c
+++ huffman.c
@@ -12,12 +12,13 @@
 #define FALSE 0
 #define ASCII_VALUES 128
 #define AUXILIARY_CHAR '&'
+#define MAX_HUFFMAN_TREE_HEIGHT 100
+
 
 typedef struct node
 {
     char character;
     unsigned int frequency;
-    int auxiliary;
     struct node *left;
     struct node *right;
 } Node;
@@ -32,18 +33,21 @@ typedef struct
 
 int valid_parameters(char *file_name, char *mode);
 void to_lower(char *str);
+int create_fd(char * file_name, int oflags, mode_t mode);
+void write_data_for_decode(int* character_frequency, int num_unique_chars);
 void encode(char *file_name);
 void decode(char *file_name);
-Node* generate_huffman_tree(int character_frequency[], int num_unique_chars);
+Node *generate_huffman_tree(int character_frequency[], int num_unique_chars);
 
 // Source for heap operations: https://www.geeksforgeeks.org/huffman-coding-greedy-algo-3/
 void heap_insert(MinHeap *min_heap, Node *node);
 Node *heap_remove();
 MinHeap *heap_init(int num_unique_chars);
-void heap_build(MinHeap* min_heap);
-void heap_print(MinHeap* min_heap);
-void heap_print_codes(Node* node, int code[], int height);
-Node *create_node(int count, char character, int auxiliary);
+void heap_build(MinHeap *min_heap);
+void heap_print(MinHeap *min_heap);
+void heap_print_codes(Node *node, char code[], int height);
+int huffman_get_code(Node *node, char c, char code[], int height);
+Node *create_node(int count, char character);
 void swap_node(Node **a, Node **b);
 void heapify(MinHeap *min_heap, int idx);
 
@@ -71,15 +75,15 @@ int main(int argc, char **argv)
 void encode(char *file_name)
 {
     int character_frequency[ASCII_VALUES] = {0};
-    int fd = open(file_name, O_RDONLY, S_IRUSR | S_IWUSR);
+    int input_file_fd = create_fd(file_name, O_RDONLY, S_IRUSR | S_IWUSR);
     struct stat sb;
-    if (fstat(fd, &sb) == -1)
+    if (fstat(input_file_fd, &sb) == -1)
     {
         perror("Couldn't retrieve file size\n");
         return;
     }
 
-    char *data = (char *)mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    char *data = (char *)mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, input_file_fd, 0);
     int num_unique_chars = 0;
     for (int i = 0; i < sb.st_size; i++)
     {
@@ -90,17 +94,27 @@ void encode(char *file_name)
         character_frequency[data[i]] += 1;
     }
 
-    Node* root = generate_huffman_tree(character_frequency, num_unique_chars);
-    int code[100];
-    heap_print_codes(root, code, 0);
+    Node *root = generate_huffman_tree(character_frequency, num_unique_chars);
+    char code[MAX_HUFFMAN_TREE_HEIGHT];
+    // heap_print_codes(root, code, 0);
+
+    int encoded_file_fd = create_fd("encode.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
+    for (int i = 0; i < sb.st_size; i++)
+    {
+        int code_len = huffman_get_code(root, data[i], code, 0);
+        write(encoded_file_fd, code, code_len);
+    }
+
+    write_data_for_decode(character_frequency, num_unique_chars);
 
     munmap(data, sb.st_size);
-    close(fd);
+    close(encoded_file_fd);
+    close(input_file_fd);
 }
 
 void decode(char *file_name) {}
 
-Node* generate_huffman_tree(int character_frequency[], int num_unique_chars)
+Node *generate_huffman_tree(int character_frequency[], int num_unique_chars)
 {
     MinHeap *min_heap = heap_init(num_unique_chars);
     int index = 0;
@@ -108,25 +122,26 @@ Node* generate_huffman_tree(int character_frequency[], int num_unique_chars)
     {
         if (character_frequency[i] > 0)
         {
-            min_heap->array[index] = create_node(character_frequency[i] /* count */, i /* char */, FALSE);
+            min_heap->array[index] = create_node(character_frequency[i] /* count */, i /* char */);
             min_heap->size += 1;
             index++;
         }
     }
     heap_build(min_heap);
-    
-    while(min_heap->size > 1) {
-        Node* left = heap_remove(min_heap);
-        Node* right = heap_remove(min_heap);
 
-        Node* auxiliary = create_node(left->frequency + right->frequency /* count */, AUXILIARY_CHAR, TRUE);
-        
+    while (min_heap->size > 1)
+    {
+        Node *left = heap_remove(min_heap);
+        Node *right = heap_remove(min_heap);
+
+        Node *auxiliary = create_node(left->frequency + right->frequency /* count */, AUXILIARY_CHAR);
+
         auxiliary->left = left;
         auxiliary->right = right;
 
         heap_insert(min_heap, auxiliary);
     }
-    
+
     return heap_remove(min_heap);
 }
 
@@ -166,44 +181,51 @@ MinHeap *heap_init(int num_unique_chars)
     return min_heap;
 }
 
-void heap_build(MinHeap* min_heap)
-{ 
-  
-    int n = min_heap->size - 1; 
-    int i; 
-  
-    for (i = (n - 1) / 2; i >= 0; --i) 
-        heapify(min_heap, i); 
+void heap_build(MinHeap *min_heap)
+{
+
+    int n = min_heap->size - 1;
+    int i;
+
+    for (i = (n - 1) / 2; i >= 0; --i)
+        heapify(min_heap, i);
 }
 
-void heap_print(MinHeap* min_heap) {
-    
-    while(min_heap->size > 0){
-        Node* node = heap_remove(min_heap);
+void heap_print(MinHeap *min_heap)
+{
+
+    while (min_heap->size > 0)
+    {
+        Node *node = heap_remove(min_heap);
         printf("Char: %c Frequency: %d\n", node->character, node->frequency);
         free(node);
     }
 }
 
-void heap_print_codes(Node* node, int code[], int height) {
-    if(node->left == NULL && node->right == NULL) {
-        // leaf
+void heap_print_codes(Node *node, char code[], int height)
+{
+    if (node->left == NULL && node->right == NULL)
+    {
+        // Leaf
         printf("Char: %c Freq: %d ", node->character, node->frequency);
-        for(int i = 0; i < height; i++) {
-            printf("%d ", code[i]);
+        for (int i = 0; i < height; i++)
+        {
+            printf("%c ", code[i]);
         }
         printf("\n");
         return;
     }
 
-    if(node->left != NULL) {
-        code[height] = 0;
-        heap_print_codes(node->left,code,height + 1);    
+    if (node->left != NULL)
+    {
+        code[height] = '0';
+        heap_print_codes(node->left, code, height + 1);
     }
 
-    if(node->right != NULL) {
-        code[height] = 1;
-        heap_print_codes(node->right,code,height + 1);
+    if (node->right != NULL)
+    {
+        code[height] = '1';
+        heap_print_codes(node->right, code, height + 1);
     }
 }
 
@@ -214,7 +236,7 @@ void heapify(MinHeap *min_heap, int idx)
     int smallest = idx;
     int left = 2 * idx + 1;
     int right = 2 * idx + 2;
-    
+
     if (left < min_heap->size && min_heap->array[left]->frequency < min_heap->array[smallest]->frequency)
         smallest = left;
 
@@ -228,7 +250,7 @@ void heapify(MinHeap *min_heap, int idx)
     }
 }
 
-Node *create_node(int count, char character, int auxiliary)
+Node *create_node(int count, char character)
 {
     Node *node = (Node *)malloc(sizeof(Node));
     if (node == NULL)
@@ -238,10 +260,43 @@ Node *create_node(int count, char character, int auxiliary)
     }
     node->frequency = count;
     node->character = character;
-    node->auxiliary = auxiliary;
     node->left = NULL;
     node->right = NULL;
     return node;
+}
+
+int huffman_get_code(Node *node, char c, char code[], int height) {
+    if (node->left == NULL && node->right == NULL)
+    {
+        // Leaf
+        if(node->character == c) {
+            return height;
+        }
+        return 0;
+    }
+
+    code[height] = '0';
+    int code_len = huffman_get_code(node->left, c, code, height + 1);
+    if(code_len == 0) {
+        code[height] = '1';
+        code_len = huffman_get_code(node->right, c, code, height + 1);
+    }
+
+    return code_len;
+}
+
+void write_data_for_decode(int* character_frequency, int num_unique_chars){
+    int fd = create_fd("decode-data.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
+    int output_buffer[ASCII_VALUES + 2];
+    int i;
+    for(i = 0; i < ASCII_VALUES; i++) {
+        dprintf(fd,"%d", character_frequency[i]);
+        if(i + 1 < ASCII_VALUES){
+            dprintf(fd," ");
+        }
+    }
+    dprintf(fd, "\n%d",num_unique_chars);
+    close(fd);
 }
 
 void swap_node(Node **a, Node **b)
@@ -261,7 +316,7 @@ int valid_parameters(char *file_name, char *mode)
     }
 
     // Check file exists
-    int fd = open(file_name, O_RDONLY, S_IRUSR | S_IWUSR);
+    int fd = create_fd(file_name, O_RDONLY, S_IRUSR | S_IWUSR);
     if (fd != -1)
     {
         close(fd);
@@ -280,4 +335,19 @@ void to_lower(char *str)
     {
         str[i] = tolower(str[i]);
     }
+}
+
+int create_fd(char * file_name, int oflags, mode_t mode) {
+    int fd;
+    if(!mode){
+        fd = open(file_name, oflags);
+    }else{
+        fd = open(file_name, oflags, mode);
+    }
+
+    if(fd == -1) {
+        fprintf(stderr, "Could not create file descriptor for %s\n", file_name);
+        exit(EXIT_FAILURE);
+    }
+    return fd;
 }
